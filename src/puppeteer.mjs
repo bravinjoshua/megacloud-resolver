4569c6842bd89a03a5088ff11645e1934162e0f9
BravinJoshua <97509851+BravinJoshua@users.noreply.github.com>
2025-01-26 11:32:52 +0300

    why is it this sty

--- src/puppeteer.mjs
+++ src/puppeteer.mjs
@@ -12,7 +12,7 @@ const js = readFileSync(join(__dirname, "mega.js"), "utf-8");
 export async function MegaCloud(url) {
   puppeteer.use(StealthPlugin());
   const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/google-chrome",
+    // executablePath: "/usr/bin/google-chrome",
     headless: true,
     devtools: false,
     args: [
